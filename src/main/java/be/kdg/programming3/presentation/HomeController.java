76558f6706f716f26a1f73e46417f3e4e382df61
Peter Buschenreiter <peter.buschenreiter@student.kdg.be>
2022-10-27 14:05:58 +0200

    feat: error page implemented

--- src/main/java/be/kdg/programming3/presentation/HomeController.java
+++ src/main/java/be/kdg/programming3/presentation/HomeController.java
@@ -5,9 +5,12 @@ import be.kdg.programming3.service.ChannelService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.time.format.DateTimeFormatter;
@@ -30,5 +33,12 @@ public class HomeController {
 		modelAndView.addObject("channels", channelService.getChannels());
 		modelAndView.addObject("dateFormatter", DateTimeFormatter.ofPattern("d. LLLL yyyy"));
 		return modelAndView;
+	}
+
+	@GetMapping ("/error")
+	@ResponseStatus (value = HttpStatus.NOT_FOUND)
+	public ModelAndView showErrorView(HttpHeaders status) {
+		logger.info(String.format("Controller is running showErrorView with status %s", status));
+		return new ModelAndView("error").addObject("status", status);
 	}
 }
