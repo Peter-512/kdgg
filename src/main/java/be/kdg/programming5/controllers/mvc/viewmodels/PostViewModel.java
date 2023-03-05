97ba77621e63a3adccf5f62a9b18a87e9f34b94a
Peter Buschenreiter <peter.buschenreiter@student.kdg.be>
2023-03-05 12:12:27 +0100

    added lombok annotations to PostViewModel

--- src/main/java/be/kdg/programming5/controllers/mvc/viewmodels/PostViewModel.java
+++ src/main/java/be/kdg/programming5/controllers/mvc/viewmodels/PostViewModel.java
@@ -1,12 +1,16 @@
 package be.kdg.programming5.controllers.mvc.viewmodels;
 
 import be.kdg.programming5.model.User;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
+@NoArgsConstructor
+@AllArgsConstructor
 @Getter
 @Setter
 public class PostViewModel {
