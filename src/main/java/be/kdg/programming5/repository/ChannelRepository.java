170fc6b61d2079a72cd0343f0e9474ed6b9a2f22
Peter Buschenreiter <peter.buschenreiter@student.kdg.be>
2023-03-25 16:27:40 +0100

    added join fetch methods for eager loading

--- src/main/java/be/kdg/programming5/repository/ChannelRepository.java
+++ src/main/java/be/kdg/programming5/repository/ChannelRepository.java
@@ -2,8 +2,11 @@ package be.kdg.programming5.repository;
 
 import be.kdg.programming5.model.Channel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ChannelRepository extends JpaRepository<Channel, Long> {
+	@Query ("select c from Channel c left join fetch c.users where c.name = :name")
+	Channel findChannelWithUsersByName(String name);
 }
--- src/main/java/be/kdg/programming5/repository/UserRepository.java
+++ src/main/java/be/kdg/programming5/repository/UserRepository.java
@@ -2,10 +2,18 @@ package be.kdg.programming5.repository;
 
 import be.kdg.programming5.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+	@Modifying
+	long deleteByName(String name);
+
+	@Query ("select u from User u left join fetch u.channels where u.name = :user")
+	User findUserWithChannelsByName(String user);
+
 	boolean existsByName(String name);
 
 	User findUserByName(String name);
