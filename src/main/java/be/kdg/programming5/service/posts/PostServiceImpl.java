d07e0bd2d47e48ca88872c1e404d5def0aeffac8
Peter Buschenreiter <peter.buschenreiter@student.kdg.be>
2023-03-25 16:22:31 +0100

    all service methods @Transactional

--- src/main/java/be/kdg/programming5/service/posts/PostServiceImpl.java
+++ src/main/java/be/kdg/programming5/service/posts/PostServiceImpl.java
@@ -7,6 +7,7 @@ import be.kdg.programming5.model.User;
 import be.kdg.programming5.repository.PostRepository;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
 
@@ -17,31 +18,37 @@ public class PostServiceImpl implements PostService {
 	public PostServiceImpl(PostRepository postRepository) {this.postRepository = postRepository;}
 
 	@Override
+	@Transactional
 	public List<Post> getPosts() {
 		return postRepository.findAll();
 	}
 
 	@Override
+	@Transactional
 	public Optional<Post> getPost(Long id) {
 		return postRepository.findById(id);
 	}
 
 	@Override
+	@Transactional
 	public Post addPost(User user, Channel channel, String content) {
 		return postRepository.save(new Post(user, channel, content));
 	}
 
 	@Override
+	@Transactional
 	public void deletePost(Long id) {
 		postRepository.deleteById(id);
 	}
 
 	@Override
+	@Transactional
 	public void setPostUpvoteCount(Long postID, int upVotes) {
 		postRepository.updateUpVotesByPostID(upVotes, postID);
 	}
 
 	@Override
+	@Transactional
 	public Post updatePost(Long id, String content) {
 		final Post post = postRepository.findById(id).orElseThrow(() -> new PostNotFoundException(id));
 		post.setContent(content);
--- src/main/java/be/kdg/programming5/service/users/UserServiceImpl.java
+++ src/main/java/be/kdg/programming5/service/users/UserServiceImpl.java
@@ -10,54 +10,62 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
 @Service
-public class UserServiceHibernate implements UserService {
+public class UserServiceImpl implements UserService {
 	private final UserRepository userRepository;
 	private final PostRepository postRepository;
 	private final BCryptPasswordEncoder passwordEncoder;
 
 	@Autowired
-	public UserServiceHibernate(UserRepository userRepository, PostRepository postRepository, BCryptPasswordEncoder passwordEncoder) {
+	public UserServiceImpl(UserRepository userRepository, PostRepository postRepository, BCryptPasswordEncoder passwordEncoder) {
 		this.userRepository = userRepository;
 		this.postRepository = postRepository;
 		this.passwordEncoder = passwordEncoder;
 	}
 
 	@Override
+	@Transactional
 	public List<User> getUsers() {
 		return Lists.newArrayList(userRepository.findAll());
 	}
 
 	@Override
+	@Transactional
 	public Optional<User> getUser(Long id) {
 		return userRepository.findById(id);
 	}
 
 	@Override
+	@Transactional
 	public Optional<User> getUser(String name) {
 		return Optional.of(userRepository.findUserByName(name));
 	}
 
 	@Override
+	@Transactional
 	public User addUser(String name, LocalDate birthdate, Role role, String password) {
 		return userRepository.save(new User(name, birthdate, role, passwordEncoder.encode(password)));
 	}
 
 	@Override
+	@Transactional
 	public void deleteUser(Long id) {
 		userRepository.deleteById(id);
 	}
 
 	@Override
+	@Transactional
 	public long getPostsCountOfUser(long userID) {
 		return postRepository.countByUser_UserID(userID);
 	}
 
 	@Override
+	@Transactional
 	public User updateUser(Long id, String name, LocalDate birthdate, Role role) {
 		return userRepository.findById(id).map(user -> {
 			user.setName(name);
