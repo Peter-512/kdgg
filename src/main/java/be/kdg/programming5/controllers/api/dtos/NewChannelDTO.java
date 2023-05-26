2168bb2be7e469d571117b45ac59d5b1ad7b66b2
Peter Buschenreiter <peter.buschenreiter@student.kdg.be>
2023-05-26 16:04:09 +0200

    use of @Data annotation for conciseness

--- src/main/java/be/kdg/programming5/controllers/api/dtos/NewChannelDTO.java
+++ src/main/java/be/kdg/programming5/controllers/api/dtos/NewChannelDTO.java
@@ -1,17 +1,11 @@
 package be.kdg.programming5.controllers.api.dtos;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.Data;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
-@NoArgsConstructor
-@AllArgsConstructor
-@Getter
-@Setter
+@Data
 public class NewChannelDTO {
 	@NotBlank (message = "Name cannot be empty")
 	@Size (min = 3, max = 25, message = "Name size must be between 3 and 25 characters")
