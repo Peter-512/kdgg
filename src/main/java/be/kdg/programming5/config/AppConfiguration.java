a66aed1f8ab835e1c6bf32ccc18ba4c4b7d303d8
Peter Buschenreiter <peter.buschenreiter@student.kdg.be>
2023-05-26 16:05:28 +0200

    config for async

---
+++ src/main/java/be/kdg/programming5/config/AppConfiguration.java
@@ -0,0 +1,9 @@
+package be.kdg.programming5.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@Configuration
+@EnableAsync
+public class AppConfiguration {
+}
